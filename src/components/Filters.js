46d1a39069cf52fc472086a91febe03dbcb944a4
Lucas-PC <lucasmvnascimento@gmail.com>
2021-03-15 17:52:02 -0300

    Starting Req 03

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,7 +6,7 @@ function Filters() {
   const { filters, setFilters } = useContext(StarWarsPlanetsContext);
   const { name } = filters.filterByName;
   return (
-    <section>
+    <form>
       <input
         type="text"
         data-testid="name-filter"
@@ -16,7 +16,26 @@ function Filters() {
           filterByName: { name: e.target.value },
         }) }
       />
-    </section>
+      <br />
+      <select
+        data-testid="column-filter"
+        // value={}
+        // onChange={}
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select data-testid="comparison-filter">
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input type="number" data-testid="value-filter" placeholder="Número" />
+      <button type="button" data-testid="button-filter">Adicionar</button>
+    </form>
   );
 }
 
